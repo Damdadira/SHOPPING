2097d9d093b0fc877530463bd66a8ddafa55545e
Damdadira <suk971029@gmail.com>
2022-07-19 18:51:52 +0900

    JS: Toggle button(Small screen)

--- main.js
+++ main.js
@@ -10,3 +10,12 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 });
+
+//navbar에서 선택하면 해당 페이지로 이동(구현X)
+
+//small screen에서 toggle button 작동하게
+const navbarMenu = document.querySelector('.navbar__menu-center');
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
