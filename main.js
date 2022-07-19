fc4a5e40a6e2116a9a3181f3d2282665ef64daec
Damdadira <suk971029@gmail.com>
2022-07-19 16:53:58 +0900

    JS: Navbar Transparent

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//navbar 투명하게 하다가, 내려오면 색상 출력
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
